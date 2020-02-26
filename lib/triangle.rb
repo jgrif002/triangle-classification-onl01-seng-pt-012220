5d9245f9b2ce34266576b9efb07c995c365f5d3b
Jesse Griffin <jesse.griffin@gmail.com>
2020-02-26 03:51:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,7 +22,7 @@ class Triangle
       raise TriangleError 
     end 
     
-    if sideA == sideB && sideB = sideC && sideA == sideC
+    if sideA == sideB && sideB == sideC 
       :equilateral 
     elsif sideA != sideB && sideB != sideC && sideA != sideC
       :scalene
