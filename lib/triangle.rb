5256d6a910e93edd56f75c181f87b0f4073ae0a1
Jesse Griffin <jesse.griffin@gmail.com>
2020-02-26 02:50:22 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,12 @@
 class Triangle
+  attr_accessor :sideA, :sideB, :sideC 
+  
+  def initiliaze(sideA, sideB, sideC)
+    @sideA = sideA
+    @sideB = sideB
+    @sideC = sideC
+  end 
+  
+  
   
 end
